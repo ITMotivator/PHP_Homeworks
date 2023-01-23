613ce09ec0c0f1f13c51d8e968fbded1d01b7b58
Vyacheslav <slava-russia@mail.ru>
2023-01-23 15:37:57 +0700

    First task done

--- HW2.php
+++ HW2.php
@@ -1,1 +1,24 @@
 <?php
+
+//Задание 1
+$correctAnswer = 1380;
+
+placeToReturn:
+print("В каком году произошла Куликовская битва?
+- 1342
+- 1380
+- 1410\n");
+
+$userAnswer = (int) readline();
+
+switch (true) {
+    case $userAnswer === 1380:
+        echo "Поздравляю, это правильный ответ!";
+        break;
+    case $userAnswer === 1342 || $userAnswer === 1410:
+        echo "К сожалению, вы ответили не верно!";
+        break;
+    default:
+        echo "Такого варианта нет в списке. Еще раз...\n";
+        goto placeToReturn;
+};
