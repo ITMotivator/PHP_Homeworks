6e53a85a6fdef5152c2770f8ee36f68dadca6774
Vyacheslav <slava-russia@mail.ru>
2023-01-23 17:14:07 +0700

    Task 3 completed

--- HW2.php
+++ HW2.php
@@ -1,27 +1,28 @@
 <?php
 
 //Задание 1
-// $correctAnswer = 1380;
+$correctAnswer = 1380;
 
-// placeToReturn:
-// print("В каком году произошла Куликовская битва?
-// - 1342
-// - 1380
-// - 1410\n");
+placeToReturn:
+print("В каком году произошла Куликовская битва?
+- 1342
+- 1380
+- 1410\n");
 
-// $userAnswer = (int) readline();
+$userAnswer = (int) readline();
 
-// switch (true) {
-//     case $userAnswer === 1380:
-//         echo "Поздравляю, это правильный ответ!";
-//         break;
-//     case $userAnswer === 1342 || $userAnswer === 1410:
-//         echo "К сожалению, вы ответили не верно!";
-//         break;
-//     default:
-//         echo "Такого варианта нет в списке. Еще раз...\n";
-//         goto placeToReturn;
-// };
+switch (true) {
+    case $userAnswer === 1380:
+        echo "Поздравляю, это правильный ответ!\n";
+        break;
+    case $userAnswer === 1342 || $userAnswer === 1410:
+        echo "К сожалению, вы ответили не верно!\n";
+        break;
+    default:
+        echo "Такого варианта нет в списке. Еще раз...\n";
+        goto placeToReturn;
+}
+;
 
 //Задание 2
 $tasksNumber = readline("Сколько дел запланировано на сегодня?\n");
@@ -31,7 +32,7 @@ while (!((int) $tasksNumber > 0 || $tasksNumber === '0')) {
 }
 
 if ($tasksNumber === '0') {
-    echo "Сегодня вы полностью свободны!";
+    echo "Сегодня вы полностью свободны!\n";
 } else {
     (int) $tasksNumber;
     $finalMessage = "Cегодня у вас запланировано задач на день: {$tasksNumber}\n";
@@ -45,6 +46,24 @@ if ($tasksNumber === '0') {
         $totalDuration += $taskDuration;
         $finalMessage .= "- $task(${taskDuration}ч)\n";
     }
-    $finalMessage .= "Примерное время выполнения плана = {$totalDuration}ч.";
+    $finalMessage .= "Примерное время выполнения плана = {$totalDuration}ч.\n";
     print($finalMessage);
 }
+
+//Задание 3
+$number = (int) readline("Введите любое положительное число\n");
+while (!((int) $number > 0)) {
+    echo "Ошибка!\n";
+    $number = (int) readline("Введите любое положительное число\n");
+}
+
+if ($number == 1 || ($number - 1) % 8 == 0) {
+    echo "Это большой палец";
+} elseif ($number == 2 || $number % 8 == 0 || ($number + 6) % 8 == 0) {
+    echo "Это указательный палец";
+} elseif ($number == 4 || ($number - 2) % 4 == 0 || ($number + 8) % 4 == 0) {
+    echo "Это безымянный палец";
+} elseif ($number == 5 || ($number - 5) % 8 == 0) {
+    echo "Это мизинец";
+} else
+    echo "Это средний палец";
