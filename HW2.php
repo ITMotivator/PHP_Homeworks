ee5d084b9447123bd8f6f1004950c18619335c4c
Vyacheslav <slava-russia@mail.ru>
2023-01-24 13:23:20 +0700

    Homework 2 corrected

--- HW2.php
+++ HW2.php
@@ -1,61 +1,59 @@
 <?php
 
 //Задание 1
-$correctAnswer = 1380;
+// $correctAnswer = 1380;
 
-placeToReturn:
-print("В каком году произошла Куликовская битва?
-- 1342
-- 1380
-- 1410\n");
+// placeToReturn:
+// print("В каком году произошла Куликовская битва?
+// - 1342
+// - 1380
+// - 1410\n");
 
-$userAnswer = (int) readline();
+// $userAnswer = (int) readline();
 
-switch (true) {
-    case $userAnswer === 1380:
-        echo "Поздравляю, это правильный ответ!\n";
-        break;
-    case $userAnswer === 1342 || $userAnswer === 1410:
-        echo "К сожалению, вы ответили не верно!\n";
-        break;
-    default:
-        echo "Такого варианта нет в списке. Еще раз...\n";
-        goto placeToReturn;
-}
-;
+// switch (true) {
+//     case $userAnswer === 1380:
+//         echo "Поздравляю, это правильный ответ!\n";
+//         break;
+//     case $userAnswer === 1342 || $userAnswer === 1410:
+//         echo "К сожалению, вы ответили не верно!\n";
+//         break;
+//     default:
+//         echo "Такого варианта нет в списке. Еще раз...\n";
+//         goto placeToReturn;
+// }
+// ;
 
 //Задание 2
-$tasksNumber = readline("Сколько дел запланировано на сегодня?\n");
-while (!((int) $tasksNumber > 0 || $tasksNumber === '0')) {
-    echo "Введите корректное значение\n";
-    $tasksNumber = readline("Сколько дел запланировано на сегодня?\n");
-}
+// $tasksNumber = readline("Сколько дел запланировано на сегодня?\n");
+// while (!((int) $tasksNumber > 0 || $tasksNumber === '0')) {
+//     echo "Введите корректное значение\n";
+//     $tasksNumber = readline("Сколько дел запланировано на сегодня?\n");
+// }
 
-if ($tasksNumber === '0') {
-    echo "Сегодня вы полностью свободны!\n";
-} else {
-    (int) $tasksNumber;
-    $finalMessage = "Cегодня у вас запланировано задач на день: {$tasksNumber}\n";
-    $totalDuration = 0;
-    $taskDuration;
-    $task;
+// if ($tasksNumber === '0') {
+//     echo "Сегодня вы полностью свободны!\n";
+// } else {
+//     $tasksNumber = (int) $tasksNumber;
+//     $finalMessage = "Cегодня у вас запланировано задач на день: {$tasksNumber}\n";
+//     $totalDuration = 0;
+//     $taskDuration = 0;
+//     $task = "";
 
-    for ($i = 1; $i <= $tasksNumber; $i++) {
-        $task = readline("Задача №$i?\n");
-        $taskDuration = (int) readline("Сколько примерно времени займет эта задача?\n");
-        $totalDuration += $taskDuration;
-        $finalMessage .= "- $task(${taskDuration}ч)\n";
-    }
-    $finalMessage .= "Примерное время выполнения плана = {$totalDuration}ч.\n";
-    print($finalMessage);
-}
+//     for ($i = 1; $i <= $tasksNumber; $i++) {
+//         $task = readline("Задача №$i?\n");
+//         $taskDuration = (int) readline("Сколько примерно времени займет эта задача?\n");
+//         $totalDuration += $taskDuration;
+//         $finalMessage .= "- $task(${taskDuration}ч)\n";
+//     }
+//     $finalMessage .= "Примерное время выполнения плана = {$totalDuration}ч.\n";
+//     print($finalMessage);
+// }
 
 //Задание 3
-$number = (int) readline("Введите любое положительное число\n");
-while (!((int) $number > 0)) {
-    echo "Ошибка!\n";
+do {
     $number = (int) readline("Введите любое положительное число\n");
-}
+} while ($number <= 0);
 
 if ($number == 1 || ($number - 1) % 8 == 0) {
     echo "Это большой палец";
