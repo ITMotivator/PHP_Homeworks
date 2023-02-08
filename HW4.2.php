020ebf3ecddde8dfaa17cf781080f878216231ea
Vyacheslav <slava-russia@mail.ru>
2023-02-08 13:33:56 +0700

    Task 4.2 corrected

--- HW4.2.php
+++ HW4.2.php
@@ -4,16 +4,9 @@ $arr = [4, 5, 1, 4, 7, 8, 15, 6, 71, 45, 2];
 function getMaxMinAvg(array $array): array
 {
     $arrRes = [];
-    $min = $array[0];
-    $max = $array[0];
-    $sum = 0;
-    foreach ($array as $value) {
-        if ($value < $min)
-            $min = $value;
-        if ($value > $max)
-            $max = $value;
-        $sum += $value;
-    }
+    $min = min($array);
+    $max = max($array);
+    $sum = array_sum($array);
     $avg = round($sum / count($array), 2);
     return $arrRes = ['max' => $max, 'min' => $min, 'average' => $avg];
 }
